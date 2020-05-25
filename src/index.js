d0946d764f8b9dc822ead3aa1cb40420ddda2005
Nazar Sosnovskyy <nsosnovskyy@liaisonedu.com>
2020-05-25 19:45:07 +0300

    add file-loader, add images and files in page and css styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from './Post';
 import './styles/styles.css'
+import json from './assets/json'
+import webpackLogo from './assets/webpack-logo.png';
 
-const post = new Post('webpack post title');
+const post = new Post('webpack post title', webpackLogo);
 console.log('post to string', post.toString());
 
+console.log('json', json);
